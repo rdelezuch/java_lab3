621d9bcfb98271ce6d959c8d576ce2eccc099ad2
Robert <gdx147658@student.wsb.gda.pl>
2022-01-09 10:51:23 +0100

    Ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,49 @@ public class Main {
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
+        Scanner scan = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.print("\nPodaj liczbę od 1 do 6. 0 kończy działanie: ");
+            switch (scan.nextInt())
+            {
+                case 0:
+                    System.out.println(StatusEnum.KONIEC);
+                    return;
+                case 1:
+                    System.out.println(LiczbyEnum.Jeden);
+                    break;
+                case 2:
+                    System.out.println(LiczbyEnum.Dwa);
+                    break;
+                case 3:
+                    System.out.println(LiczbyEnum.Trzy);
+                    break;
+                case 4:
+                    System.out.println(LiczbyEnum.Cztery);
+                    break;
+                case 5:
+                    System.out.println(LiczbyEnum.Pięć);
+                    break;
+                case 6:
+                    System.out.println(LiczbyEnum.Sześć);
+                    break;
+                default:
+                    System.out.println("Podałeś wartość poza przedziałem");
+                    break;
+            }
+            System.out.println(StatusEnum.KONTYNUUJEMY);
+        }
     }
+}
+
+enum LiczbyEnum
+{
+    Jeden, Dwa, Trzy, Cztery, Pięć, Sześć;
+}
+
+enum StatusEnum
+{
+    KONTYNUUJEMY, KONIEC;
 }
