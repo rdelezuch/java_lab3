a7790e872e8639418624e15ac2b8298716638a08
Robert <gdx147658@student.wsb.gda.pl>
2022-01-22 00:29:19 +0100

    Ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,22 @@ public class Main {
             e) wywołać metody suma i róznica (obie wersje) z klasy punkt
         */
 
+        Punkt p1 = new Punkt(7);
+        System.out.println(p1.toString());
+
+        Punkt p2 = new Punkt(1, 2,3);
+        System.out.println(p2.toString());
+
+        p2.setpX(4);
+        p2.setpY(5);
+        p2.setpZ(6);
+
+        System.out.println(p2.toString());
+
+        System.out.printf("x = %d, y = %d, z = %d\n", p2.getpX(), p2.getpY(), p2.getpZ());
+
+        System.out.printf("Suma: %d\n", p2.suma());
+        System.out.printf("Różnica 1: %d\n", p2.roznica());
+        System.out.printf("Różnica 2: %d\n", p2.roznica(1,2,3));
     }
 }
--- src/Punkt.java
+++ src/Punkt.java
@@ -2,6 +2,8 @@
 public class Punkt {
     /* stworzyć zmienne prywatne pX, pY, pZ */
 
+    private int pX, pY, pZ;
+
     /* stworzyć
         a) pusty konstruktor,
         b) konstruktor inicjalizujacy zmienną pX,
@@ -13,6 +15,50 @@ public class Punkt {
         konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
     */
 
+    Punkt(){}
+
+    Punkt(int pX)
+    {
+        this.pX = pX;
+    }
+
+    Punkt(int pX, int pY, int pZ)
+    {
+        this.pX = pX;
+        this.pY = pY;
+        this.pZ = pZ;
+    }
+
+    public int getpX()
+    {
+        return pX;
+    }
+
+    public int getpY()
+    {
+        return pY;
+    }
+
+    public int getpZ()
+    {
+        return pZ*10;
+    }
+
+    public void setpX(int pX)
+    {
+        this.pX = pX;
+    }
+
+    public void setpY(int pY)
+    {
+        this.pY = pY;
+    }
+
+    public void setpZ(int pZ)
+    {
+        this.pZ = pZ;
+    }
+
     /* Stworzyć metody
         a) suma - sumująca wszystkie wartości w klasie
         b) różnica - metody przeciążone roznica(){} i roznica(int x, int y, int z){}
@@ -20,4 +66,24 @@ public class Punkt {
             roznica(){ return pX - pY - pZ} i roznica(int x, int y, int z){ return pX*x - pY*y - pZ*z }
         d) nadpisać metodę toString(), aby zwracała wszystkie wartości pól, analogicznie jak w przykładzie
      */
+
+    public int suma()
+    {
+        return pY + pX + pZ;
+    }
+
+    public int roznica()
+    {
+        return pX - pY - pZ;
+    }
+
+    public int roznica(int x, int y, int z)
+    {
+        return pX*x - pY*y - pZ*z;
+    }
+
+    @Override
+    public String toString() {
+        return "Punkt{" + "x=" + pX + ", y=" + pY + ", z=" + pZ + "}";
+    }
 }
